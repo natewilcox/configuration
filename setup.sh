0facbb8200bfec27b92797669395d5d20d781270
Nathan Wilcox <natewilcox@gmail.com>
2024-10-05 10:14:55 -0400

    Add plugin configuration to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,9 @@ rm -f ~/.vimrc
 
 ln -s $(pwd)/.vimrc ~/.vimrc
 
+mkdir -p ~/.vim/pack/plugins/start
+
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp 
+git clone https://github.com/prabirshrestha/vim-lsp.git ~/.vim/pack/plugins/start/vim-lsp 
+
 exit 0
