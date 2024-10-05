1d01d854a9dfc4e7c4aaaab7af59d6a7a5db4799
Nathan Wilcox <natewilcox@gmail.com>
2024-10-05 15:49:26 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -f ~/.vimrc
+rm -rf ~/.vim/pack
 
 ln -s $(pwd)/.vimrc ~/.vimrc
 
@@ -8,5 +9,6 @@ mkdir -p ~/.vim/pack/plugins/start
 
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp 
 git clone https://github.com/prabirshrestha/vim-lsp.git ~/.vim/pack/plugins/start/vim-lsp 
+git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/pack/plugins/start/auto-pairs
 
 exit 0
