f9c477dc289994d531b37cc2ce861633556478eb
Nathan Wilcox <natewilcox@gmail.com>
2024-10-05 23:22:42 -0400

    add packages and git settings

--- setup.sh
+++ setup.sh
@@ -7,8 +7,14 @@ ln -s $(pwd)/.vimrc ~/.vimrc
 
 mkdir -p ~/.vim/pack/plugins/start
 
+sudo apt install vim git nodejs npm -y
+sudo npm install -g typescript-language-server typescript
+
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp 
 git clone https://github.com/prabirshrestha/vim-lsp.git ~/.vim/pack/plugins/start/vim-lsp 
 git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/pack/plugins/start/auto-pairs
+
+git config --global user.email "natewilcox@gmail.com"
+git config --global user.name "Nathan Wilcox"
 
 exit 0
