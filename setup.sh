eb9d3cdf3aa41998b2708d71c525f6f8b486560c
Nathan Wilcox <natewilcox@gmail.com>
2024-10-23 21:50:44 -0400

    remove nvim path config

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ mkdir -p ~/Projects
 mkdir -p ~/.vim/pack/plugins/start
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install vim nodejs npm openjdk-21-jdk -y
+sudo apt install vim nodejs npm openjdk-21-jdk neovim -y
 sudo npm install -g typescript-language-server typescript
 
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp 
@@ -22,7 +22,6 @@ git config --global user.email "natewilcox@gmail.com"
 git config --global user.name "Nathan Wilcox"
 git config --global credential.helper store
 
-export PATH="$PATH:/opt/nvim-linux64/bin"
 echo "nathan ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 
 exit 0
