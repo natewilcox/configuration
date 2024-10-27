dc96ea6ffd16e7e4439501b1c457206deab1af88
Nathan Wilcox <natewilcox@gmail.com>
2024-10-27 19:09:17 -0400

    add java

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "java_language_server" }
+                ensure_installed = { "lua_ls", "ts_ls", "jdtls" }
             })
         end
     },
@@ -19,7 +19,7 @@ return {
             local lspconfig = require('lspconfig')
             lspconfig.lua_ls.setup({})
             lspconfig.ts_ls.setup({})
-            lspconfig.java_language_server.setup({})
+            lspconfig.jdtls.setup({})
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
