8902ac26d39b8d4476f2506daffad3fa11c50840
Nathan Wilcox <natewilcox@gmail.com>
2024-10-27 19:20:28 -0400

    add java to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java" },
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
